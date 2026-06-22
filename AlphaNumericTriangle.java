c2109000a6b1288d652637e3032aff74d4f99b0b
Umair Farooq <mirumair7893@gmail.com>
2026-06-22 12:46:46 +0530

    message

--- AlphaNumericTriangle.java
+++ AlphaNumericTriangle.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
 public class AlphaNumericTriangle {
-    
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the height of triangle");
+        int m = sc.nextInt();
+
+        int n = 1;
+        while(m>=n){
+        for(int i=1 ;i<=n ;i++){
+            if(n%2!=0){
+            System.out.print(i+" ");
+            }else{
+            System.out.print((char)(64+i)+" ");
+            }
+        }
+            System.out.println();  
+        n++;
+        }
+    }
 }
--- NumTriangle.java
+++ NumTriangle.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
 public class NumTriangle {
-    
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the height of triangle");
+        int m = sc.nextInt();
+
+        int n = 1;
+        while(m>=n){
+        for(int i=1 ;i<=n ;i++){
+            System.out.print(i+" ");
+        }
+            System.out.println();  
+        n++;
+        }
+    }
 }
